448707aa525bb0a3f847cba7ba5b0c70b552cfcf
Temidayo Famiwole <famiwole4@gmail.com>
2026-08-14 07:45:55 +0100

    Updated jenkins-vault folder

---
+++ jenkin-vault/variable.tf
@@ -0,0 +1,10 @@
+
+variable "domain" {
+ 
+  description = "The domain name for pet adoption project"
+  type        = string
+  default     = "ftdmain.xyz"
+}
+variable "region" {
+  default = "eu-west-3"
+}
